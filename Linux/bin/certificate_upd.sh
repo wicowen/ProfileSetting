632c23c525727909a0729bfb830c3698a82135fe
Wico Wen <is@wico.cc>
2016-12-06 14:38:09 +0800

    add linux letsencrypt auto update

--- Linux/bin/certificate_upd.sh
+++ Linux/bin/certificate_upd.sh
@@ -3,7 +3,7 @@
 sudo service apache2 stop
 
 cd ~/Repository/letsencrypt
-./letsencrypt-auto certonly -a standalone -d bun.tw -d www.bun.tw -d dev.bun.tw -d api.bun.tw -d lumen.bun.tw -d laravel.bun.tw -d m.bun.tw --server https://acme-v01.api.letsencrypt.org/directory --agree-dev-preview
+sudo ./letsencrypt-auto certonly -a standalone -d bun.tw -d www.bun.tw -d dev.bun.tw -d api.bun.tw -d lumen.bun.tw -d laravel.bun.tw -d m.bun.tw -d seth.bun.tw  --server https://acme-v01.api.letsencrypt.org/directory
 
 sudo service apache2 start
 
