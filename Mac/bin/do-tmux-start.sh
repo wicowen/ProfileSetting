de1ae9817c0bbd862c761f6db5f57dd152725450
WicoWen <is@wico.cc>
2018-05-20 14:58:59 +0800

    change tmux initial workspace setting

--- Mac/bin/do-tmux-start.sh
+++ Mac/bin/do-tmux-start.sh
@@ -8,6 +8,9 @@ tmux send-keys 'cd ~/GitHub/node-hexo-blog' C-m
 tmux new-window -n 'frontend'
 tmux send-keys 'cd ~/GitHub/App_007' C-m
 
+tmux new-window -n 'frontend'
+tmux send-keys 'cd ~/GitHub/App_007' C-m
+
 tmux new-window -n 'backend'
 tmux send-keys 'cd ~/GitHub/App_008' C-m
 
@@ -15,10 +18,13 @@ tmux new-window -n 'php'
 tmux send-keys 'cd ~/GitHub/interview-CtCI-6th-Edition-php' C-m
 
 tmux new-window -n 'python'
-tmux send-keys 'cd ~/GitHub/Python-PTTCrawlerLibrary' C-m
+tmux send-keys 'cd ~/GitHub/' C-m
 
-tmux new-window -n 'ReactNative'
-tmux send-keys 'cd ~/GitHub/App_009' C-m
+#tmux new-window -n 'python'
+#tmux send-keys 'cd ~/GitHub/Python-PTTCrawlerLibrary' C-m
+
+#tmux new-window -n 'ReactNative'
+#tmux send-keys 'cd ~/GitHub/App_009' C-m
 
 tmux attach-session -d
 
