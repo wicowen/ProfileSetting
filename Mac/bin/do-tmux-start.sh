1fcca396757e74567704f755da6383ec9b488a22
WicoWen <is@wico.cc>
2018-06-01 13:08:00 +0800

    update mac setting

--- Mac/bin/do-tmux-start.sh
+++ Mac/bin/do-tmux-start.sh
@@ -2,11 +2,23 @@
 
 tmux new-session -d
 
+tmux new-window -n 'me'
+tmux send-keys 'cd ~/GitHub/me-market-front' C-m
+
+tmux new-window -n 'me'
+tmux send-keys 'cd ~/GitHub/me-front-laboratory' C-m
+
+tmux new-window -n 'me'
+tmux send-keys 'cd ~/GitHub/me-videoceo' C-m
+
 tmux new-window -n 'blog'
 tmux send-keys 'cd ~/GitHub/node-hexo-blog' C-m
 
-tmux new-window -n 'frontend'
-tmux send-keys 'cd ~/GitHub/App_007' C-m
+tmux new-window -n 'python'
+tmux send-keys 'cd ~/GitHub/' C-m
+
+tmux new-window -n 'php'
+tmux send-keys 'cd ~/GitHub/interview-CtCI-6th-Edition-php' C-m
 
 tmux new-window -n 'frontend'
 tmux send-keys 'cd ~/GitHub/App_007' C-m
@@ -14,11 +26,7 @@ tmux send-keys 'cd ~/GitHub/App_007' C-m
 tmux new-window -n 'backend'
 tmux send-keys 'cd ~/GitHub/App_008' C-m
 
-tmux new-window -n 'php'
-tmux send-keys 'cd ~/GitHub/interview-CtCI-6th-Edition-php' C-m
 
-tmux new-window -n 'python'
-tmux send-keys 'cd ~/GitHub/' C-m
 
 #tmux new-window -n 'python'
 #tmux send-keys 'cd ~/GitHub/Python-PTTCrawlerLibrary' C-m
